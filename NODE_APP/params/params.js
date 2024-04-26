d28902e77f9123d42111f0433b0b40bd031c502c
Hubert Veyannie V <hubertveyannie07@gmail.com>
2024-04-27 00:14:47 +0530

    Add files via upload

---
+++ NODE_APP/params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://hubertveyannie07:Veyannie@3105@cluster0.7sflh2u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+module.exports = {
+    DATABASECONNECTION
+}
