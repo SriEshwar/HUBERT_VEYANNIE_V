89371c1302db48484dadb5840da4770116c628e8
Hubert Veyannie V <hubertveyannie07@gmail.com>
2024-05-07 14:52:11 +0530

    InsertOne

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,37 @@
-var mongoClient = require('mongodb').MongoClient
-var uri = "mongodb://localhost:27017/"
-var dbname = "employee"
+const { MongoClient } = require('mongodb');
 
-console.log(uri+dbname)
+const uri = 'mongodb://127.0.0.1:27017/';
 
-mongoClient.connect(uri+dbname, function(connectError, connectEstablished){
-    if(connectError){
-        console.log("error in connecting db "+ connectError)
-    }else{
-        console.log("Connection established sucessfully")
+const client = new MongoClient(uri);
+
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+
+        const db = client.db('employee')
+        const collection = db.collection('employees')
+
+        const cursor = collection.find({})
+        const akresult = await collection.insertOne({
+            _id: 7,
+            firstName: 'Santosh',
+            lastName: 'M',
+            gender: 'male',
+            email: 'santosh.m@abc.com',
+            salary: 8000,
+            department: { name: 'Developer' }
+        })
+
+        console.log('the inserted id ' + akresult.insertedId)
+
+        await cursor.forEach(record => {
+            console.log(record)
+        })
+        await client.close()
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
     }
-})
+}
+
+connectToMongoDB();
