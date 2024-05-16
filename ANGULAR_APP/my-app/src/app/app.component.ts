39e568e0286ef5b08a849322484f24bafa2cfd29
Hubert Veyannie V <hubertveyannie07@gmail.com>
2024-05-16 20:07:14 +0530

    Routing/task

--- ANGULAR_APP/my-app/src/app/app.component.ts
+++ ANGULAR_APP/my-app/src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
-
-  clickCount = 0;
-  clickme(){
-    this.clickCount++
-  }
-  reset(){
-    this.clickCount = 0;
-  }
 }
--- ANGULAR_APP/my-app/src/app/app.routes.ts
+++ ANGULAR_APP/my-app/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactUSComponent } from './contact-us/contact-us.component';
+import { ServicesComponent } from './services/services.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'','title':'Home', component:HomeComponent},
+    {'path':'login','title':'Login',component:LoginComponent},
+    {'path':'products','title':'Products',component:ProductsComponent},
+    {'path':'services','title':'Services',component:ServicesComponent},
+    {'path':'blog','title':'Blog',component:BlogComponent},
+    {'path':'about-us','title':'About Us',component:AboutUsComponent},
+    {'path':'contact-us','title':'Contact Us',component:ContactUSComponent}
+];
