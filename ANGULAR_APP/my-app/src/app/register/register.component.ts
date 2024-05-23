2ab1dac96c01de09a48448c8e6ed436f8e70297b
Hubert Veyannie V <hubertveyannie07@gmail.com>
2024-05-23 11:33:03 +0530

    Validaiton for registrationn using Reactive forms

--- ANGULAR_APP/my-app/src/app/register/register.component.ts
+++ ANGULAR_APP/my-app/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 
@@ -15,13 +15,23 @@ export class RegisterComponent {
   hide=true;
   password='';
 
-  form={
-    fullname: '',
-    email: '',
-    password: '',
-    contact: ''
+  form: FormGroup;
+
+  isFormSubmitted: boolean = false
+
+  constructor(){
+    this.form = new FormGroup({
+      fullname: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8)]),
+      contact: new FormControl("", [Validators.required, Validators.maxLength(10), Validators.minLength(10)])
+    })
   }
+  
   onSubmit(): void{
+    this.isFormSubmitted = true
+
+
     console.log(JSON.stringify(this.form))
   }
 }
