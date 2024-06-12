7495a877b7bd92c50bb1a14ededa2ddc6e77959e
Hubert Veyannie V <hubertveyannie07@gmail.com>
2024-06-12 20:22:00 +0530

    Routing in React

--- React_Learning/demo-app/src/App.js
+++ React_Learning/demo-app/src/App.js
@@ -1,11 +1,26 @@
+import Home from './Components/Home'
+import Contactus from './Components/Contactus';
+import Products from './Components/Products';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+// import './App.css';
 
-import './App.css';
-import { Counter } from './Counter';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <h1>Hello World!</h1>
+      <BrowserRouter>
+      <ul>
+        <li><Link to='/'>Homme</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
+        <li><Link to='/products'>Products</Link></li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/contact' element={<Contactus />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ React_Learning/demo-app/src/Components/Contactus.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+function Contactus() {
+  return (
+    <div>Contact Us</div>
+  );
+}
+
+export default Contactus;
